cba48410da876efa16e73a196fa33f3a2079d0a7
ClaudiaPinaC <claudiapinac524@gmail.com>
2026-06-29 23:45:51 -0700

    PS: Greatest common strings divisor - description.

---
+++ Problem_Solving/greatest_common_strings_divisor.js
@@ -0,0 +1,11 @@
+// Greatest common divisor of Strings.
+
+// For two strings 's' and 't', we say 't' divides 's'if and only if 's = t + t + t + ... + t + t' 
+// (t is concatenated with itself one or more times).
+
+// Return the largest string 'x' such that 'x' divides the strings given. 
+
+function gcdOfStrings(str1, str2) {
+
+    
+}
