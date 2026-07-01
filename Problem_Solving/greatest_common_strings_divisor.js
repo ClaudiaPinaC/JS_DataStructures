2c632ca6e1d72837b0bbfecf73de274bf4c8edd2
ClaudiaPinaC <claudiapinac524@gmail.com>
2026-06-30 23:31:48 -0700

    PS: Greatest common string divisor - solution

--- Problem_Solving/greatest_common_strings_divisor.js
+++ Problem_Solving/greatest_common_strings_divisor.js
@@ -6,6 +6,29 @@
 // Return the largest string 'x' such that 'x' divides the strings given. 
 
 function gcdOfStrings(str1, str2) {
+    if (str1 + str2 !== str2 + str1) { // we verify if the sum of the strings are equal
+        return ""; // if it's not, we cannot create the same strings and we cannot find 'x'
+    }
 
-    
+    const gcd = (len1, len2) => { // use of Euclid's algorithm
+        while (len2 !== 0) {
+            [len1, len2] = [len2, len1 % len2];
+        }
+        return len1;
+    };
+
+    return str1.slice(0, gcd(str1.length, str2.length));
 }
+
+const str1_c1 = "ABCABC";
+const str2_c1 = "ABC";
+
+const str1_c2 = "ABABAB";
+const str2_c2 = "AB";
+
+const str1_c3 = "AAAAAAB";
+const str2_c3 = "AAA";
+
+console.log("Case 1: " + gcdOfStrings(str1_c1, str2_c1));
+console.log("Case 2: " + gcdOfStrings(str1_c2, str2_c2));
+console.log("Case 3: " + gcdOfStrings(str1_c3, str2_c3));
